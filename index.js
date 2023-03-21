0e271d5c75a0f8dc7deab655e8a376627548ca3b
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-03-21 20:33:06 +0100

    P.5 done

--- index.js
+++ index.js
@@ -7,13 +7,27 @@ function esDivisible(num1, num2) {
     } 
     else {
         return false;
-    }
-}
+    };
+};
 
 /* P.4 */
 function capitalizeFirstLetters(arr) {
     return arr.map(str => str.charAt(0).toUpperCase() + str.slice(1));
-}
+};
+
+/* P.5 */
+function calcularMediana(array) {
+    const numerosOrdenados = array.sort((a, b) => a - b);
+    const mitadArray = Math.floor(numerosOrdenados.length / 2);
+    
+    if (numerosOrdenados.length % 2 === 0) {
+        return (numerosOrdenados[mitadArray - 1] + numerosOrdenados[mitadArray]) / 2;
+    } 
+    else {
+        return numerosOrdenados[mitadArray];
+    };
+};
+
 
 
 
